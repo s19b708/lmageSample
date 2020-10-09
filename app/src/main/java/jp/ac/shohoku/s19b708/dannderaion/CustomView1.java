601cff72b9f0a0c1eb854c2ac2272aea9fb99a32
s19B708 <s19b708@shohoku.ac.jp>
2020-10-09 11:35:43 +0900

    課題のコード

--- app/src/main/java/jp/ac/shohoku/s19b708/dannderaion/CustomView1.java
+++ app/src/main/java/jp/ac/shohoku/s19b708/dannderaion/CustomView1.java
@@ -1,5 +1,46 @@
 package jp.ac.shohoku.s19b708.dannderaion;
 
-public class CustomView1 {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
 
+public class CustomView1 extends View {
+    private  Context context;
+
+    /**
+     * コンストラクタ
+     * @param context
+     * @param attrs
+     */
+    public CustomView1(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    /**
+     * 描画するメソッド
+     */
+    @Override
+    protected void onDraw(Canvas canvas){
+        super. onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+        Paint p = new Paint();
+        p.setStyle(Style.STROKE);
+        p.setColor(Color.DKGRAY);
+        canvas.drawRect(new Rect(5,5,w-10,h-10),p);
+        Resources rs = this.getContext().getResources();
+        Bitmap bmp = BitmapFactory.decodeResource(rs, R.drawable.hiyorieva);
+        canvas.drawBitmap(bmp, 0, 0, p);
+    }
 }
